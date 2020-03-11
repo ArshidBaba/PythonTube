4bd6239dbd003c9ab53b71ccfa72729f6e0141de
Arshid <arshidbaba93@gmail.com>
2020-03-11 17:33:00 +0530

    This script downloads videos from youtube

--- singlevideo.py
+++ singlevideo.py
@@ -2,10 +2,12 @@ from pytube import YouTube, Stream
 
 link = open('video_ids.txt', 'r')
 
+
+
 for i in link:
-    print(i)
+    print(i[1:12])
     try:
-        yt = YouTube("https://www.youtube.com/watch?v="+i)
+        yt = YouTube("https://www.youtube.com/watch?v="+i[1:12])
         print(yt.title)
 
         print(yt.thumbnail_url)
@@ -21,6 +23,9 @@ for i in link:
         stream.download(output_path='/home/arshid/Hash/youtube/lectures', filename=yt.title+"_"+i)
         # stream.download(output_path='/home/ubuntu/hashlearn/lectures', filename=yt.title+"_"+i)
         print("Downloaded "+yt.title+" successfully")
+        link2 = open('successful_video_ids.txt', "a")
+        link2.write(i)
+        link2.close()
     except Exception as e:
         print("An Exception Occured: " + str(e))
         failed_link = open("failed_downloads.txt","a")
